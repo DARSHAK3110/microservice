be9a38de34ce872c26eef79311f97c6abf4d965f
DARSHAK3110 <darshakzadafiya2019@gecg28.ac.in>
2023-07-21 10:28:09 +0530

    oauth dependency remove

--- Library/src/main/java/com/training/library/controllers/AuthorController.java
+++ Library/src/main/java/com/training/library/controllers/AuthorController.java
@@ -3,7 +3,6 @@ package com.training.library.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.training.library.dto.request.AuthorRequestDto;
 import com.training.library.dto.response.AuthorResponseDto;
@@ -34,7 +32,6 @@ public class AuthorController {
 		return ResponseEntity.ok(author);
 	}
 
-	@ResponseStatus(code = HttpStatus.UNAUTHORIZED, reason = "Token Expired", value = HttpStatus.UNAUTHORIZED)
 	@GetMapping
 	public ResponseEntity<List<AuthorResponseDto>> findAuthors() {
 		List<AuthorResponseDto> authors = authorService.findAuthors();
